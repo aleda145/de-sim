fc11747ef00da1277acfa9b36d0a7638cb52ed02
Alexander Dahl <alex@dahl.dev>
2023-08-05 02:26:36 +0200

    Add burnout store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { burnout } from './stores';
 </script>
 
 <svelte:head>
@@ -20,7 +21,7 @@
 
 		to your new<br />SvelteKit app
 	</h1>
-
+	Current burnout: {$burnout}
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
