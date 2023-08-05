3e20a7614a7cd384eb47bea14fd7ebc06e1cf9ab
Alexander Dahl <alex@dahl.dev>
2023-08-05 04:06:04 +0200

    use localstorage

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,3 +1,13 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+// Get the value out of storage on load.
+let stored = 0
+if (browser){
 
-export const burnout = writable(50);
+    stored = localStorage.burnout
+}
+export const burnout = writable(stored || 75);
+
+if (browser) {
+    burnout.subscribe((value) => localStorage.burnout = value)
+}
