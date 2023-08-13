67c69256e597925494662010a9abc9a19b050907
Alexander Dahl <alex@dahl.dev>
2023-08-13 06:56:57 +0200

    Add burnout change

--- src/routes/Burnout.svelte
+++ src/routes/Burnout.svelte
@@ -2,14 +2,15 @@
 	import { burnout } from './stores';
 	/**
 	 * @param {number} burn
+	 * @param {number} change
 	 */
-	function burnoutMath(burn) {
-		return burn + 1;
+	function burnoutMath(burn, change) {
+		return burn + change;
 	}
 </script>
 
 <button
-	on:click={() => ($burnout = burnoutMath($burnout))}
+	on:click={() => ($burnout = burnoutMath($burnout, 1))}
 	aria-label="Decrease the counter by one"
 >
 	<svg aria-hidden="true" viewBox="0 0 1 1">
